436dc9043b4b462588e9fb20cba7cde9943cc48a
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-04-25 22:23:24 +0500

    first commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,10 @@
 import icons from "url:../img/icons.svg";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import recipeView from './views/recipeView.js';
-import searchView from './views/searchView.js';
-import resultsView from './views/resultsView.js';
-import paginationView from './views/paginationView.js';
+// import recipeView from './views/recipeView.js';
+// import searchView from './views/searchView.js';
+// import resultsView from './views/resultsView.js';
+// import paginationView from './views/paginationView.js';
 console.log(icons);
 const recipeContainer = document.querySelector(".recipe");
 
