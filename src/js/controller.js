d5cd7c71db6d677ce8fc3ce2fc851b2f52bb88a9
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-07-13 17:16:05 +0500

    implement error and success message

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,14 +25,13 @@ const controlRecipes = async function () {
     recipeView.render(model.state.recipe);
     // const recipeView=new recipeView(model.state.recipe)
   } catch (err) {
-    alert(err);
+    recipeView.renderError(
+      "No recipes found for your query. Please try again!"
+    );
   }
 };
 
-console.log("TESAT");
-// controlRecipes();
-
-// ['hashchange','load'].forEach(ev=> window.addEventListener(ev,controlRecipes))
-
-window.addEventListener("hashchange", controlRecipes);
-window.addEventListener("load", controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,8 @@ console.log(Fraction);
 class recipeView {
   _parentElement = document.querySelector(".recipe");
   _data;
+  _errorMessage = "No recipes found for your query. Please try again!";
+  _message = "";
   render(data) {
     this._data = data;
     const markup = this._generateMarkup();
@@ -13,7 +15,7 @@ class recipeView {
   _clear() {
     this._parentElement.innerHTML = "";
   }
-  renderSpinner = function () {
+  renderSpinner() {
     const markup = `
     <div class="spinner">
     <svg>
@@ -21,9 +23,43 @@ class recipeView {
     </svg>
   </div> 
   `;
-    this._parentElement.innerHTML = "";
+    this._clear();
     this._parentElement.insertAdjacentHTML("afterbegin", markup);
-  };
+  }
+  renderError(message = this._errorMessage) {
+    const markup = `
+     <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+           `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+  renderMessage(message = this._message) {
+    const markup = `
+     <div class="message">
+            <div>
+              <svg>
+                <use href="${icons}#icon-smile"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+           `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  addHandlerRender(handler) {
+    ["hashchange", "load"].forEach((ev) =>
+      window.addEventListener(ev, handler)
+    );
+  }
 
   _generateMarkup() {
     return `
